1ce5aeac016a79f5c01a95913eb9169d6c4e9110
like-programmer <dashutkadevi@gmail.com>
2020-07-16 16:16:49 +0300

    3.11 add artist-question-screen

---
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {GameType} from "../../const.js";
+
+export const ArtistQuestionScreen = (props) => {
+  return ();
+};
+
+ArtistQuestionScreen.propTypes = {
+  onAnswer: PropTypes.func.isRequired,
+  question: PropTypes.shape({
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+    })).isRequired,
+    song: PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    }).isRequired,
+    type: PropTypes.oneOf([GameType.GENRE, GameType.ARTIST]).isRequired,
+  }).isRequired,
+};
