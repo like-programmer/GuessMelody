cf8037fc4f9e5ba059f79af8407d22f3ad2efa4f
like-programmer <dashutkadevi@gmail.com>
2020-07-16 18:07:57 +0300

    3.17 add router & describe paths

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,10 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+import {GameType} from "../../const.js";
 
 class App extends PureComponent {
   constructor(props) {
@@ -8,13 +12,29 @@ class App extends PureComponent {
   }
 
   render() {
-    const {errorCount} = this.props;
+    const {questions} = this.props;
 
-    return <WelcomeScreen
-      errorCount={errorCount}
-      onWelcomeBtnClick={() => {
-      }}
-    />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/artist">
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path="/genre">
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
