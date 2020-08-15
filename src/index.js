983aabb8107abb221447c70959656371b7a6b0c4
like-programmer <dashutkadevi@gmail.com>
2020-08-15 22:54:40 +0300

    7.10 return redux devTools support. Use composeWithDevTools which contains _REDUX_DEVTOOLS_EXTENSION_ as middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {createStore, applyMiddleware} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 import App from "./components/app/app.jsx";
@@ -17,7 +18,9 @@ const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
-    applyMiddleware(thunk.withExtraArgument(api))
+    composeWithDevTools(
+            applyMiddleware(thunk.withExtraArgument(api))
+        )
 );
 
 store.dispatch(DataOperation.loadQuestions());
