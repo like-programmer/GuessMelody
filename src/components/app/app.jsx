b16b7bdaf4210d4f4aa7b721cbaace9284c32f3b
like-programmer <dashutkadevi@gmail.com>
2020-08-23 20:50:26 +0300

    8.6 replace components render with history.push() in _renderGameScreen()

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -32,13 +32,11 @@ class App extends PureComponent {
   _renderGameScreen() {
     const {
       authorizationStatus,
-      login,
       mistakes,
       maxMistakes,
       questions,
       onUserAnswer,
       onWelcomeBtnClick,
-      resetGame,
       step,
     } = this.props;
     const question = questions[step];
@@ -53,29 +51,14 @@ class App extends PureComponent {
     }
 
     if (mistakes >= maxMistakes) {
-      return (
-        <GameOverScreen
-          onReplayBtnClick={resetGame}
-        />
-      );
+      return history.push(AppRoute.LOSE);
     }
 
     if (step >= questions.length) {
       if (authorizationStatus === AuthorizationStatus.AUTH) {
-        return (
-          <WinScreen
-            questionsCount={questions.length}
-            mistakesCount={mistakes}
-            onReplayButtonClick={resetGame}
-          />
-        );
+        return history.push(AppRoute.RESULT);
       } else if (authorizationStatus === AuthorizationStatus.NO_AUTH) {
-        return (
-          <AuthScreen
-            onReplayButtonClick={resetGame}
-            onSubmit={login}
-          />
-        );
+        return history.push(AppRoute.LOGIN);
       }
 
       return null;
